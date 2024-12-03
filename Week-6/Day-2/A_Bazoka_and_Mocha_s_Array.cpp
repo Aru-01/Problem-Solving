09c50f42ab9abc785812c98a89c63f902179a675
arifu00 <arifu5758@gmail.com>
2024-12-03 23:16:07 +0600

    added Week->6 : Day->2

---
+++ Week-6/Day-2/A_Bazoka_and_Mocha_s_Array.cpp
@@ -0,0 +1,28 @@
+// problem: https://codeforces.com/problemset/problem/1975/A
+
+#include <bits/stdc++.h>
+#define ll long long
+#define no cout << "NO\n"
+#define yes cout << "YES\n"
+using namespace std;
+void solve()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int &i : arr)
+            cin >> i;
+        
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    return 0;
+}
